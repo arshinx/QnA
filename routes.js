18e168d184420533028ebb3f3880bc791bae406d
Arshin Jain <arshin.jain@outlook.com>
2016-07-18 18:39:57 -0700

    Begin router for routes

--- routes.js
+++ routes.js
@@ -3,4 +3,10 @@
 var express = require("express");
 var router = express.Router();
 
+// GET /questions
+router.get("/", function(req, res) {
+
+    // Return All Questions
+});
+
 module.exports = router;
