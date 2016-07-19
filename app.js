d2f590777c4ce1a37d4b936c92582470209475bd
Arshin Jain <arshin.jain@outlook.com>
2016-07-18 17:51:56 -0700

    Remove Middlewares

--- app.js
+++ app.js
@@ -4,24 +4,8 @@ var express = require('express');
 var app = express();
 var jsonParser = require('body-parser').json;
 
-// Configure Middleware - DRY Principle
-var jsonCheck = function(req, res, next) {
-    if (req.body) {
-        console.log("The Sky is", req.body.color);
-    } else {
-        console.log("There is no body property on the request");
-    }
-    next();
-}
-
-// First Middleware
-app.use(jsonCheck);
-
 // Use Modules with Express
 app.use(jsonParser());
-
-// Configure Second Middleware
-app.use(jsonCheck);
 
 var port = process.env.PORT || 3000;
 
