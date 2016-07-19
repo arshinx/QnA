03dbb8f703eaef4804c2d353ea186e283ed16b6a
Arshin Jain <arshin.jain@outlook.com>
2016-07-19 13:13:35 -0700

    rename user to use

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(function(req, res, next){
 });
 
 // Error Handler -- Extra Parameter helps differentiate between Error Handler and Middleware
-app.user(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.json({
         error: {
