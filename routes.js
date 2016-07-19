466d2de887ee618f70a477d982f497d69704c4ee
Arshin Jain <arshin.jain@outlook.com>
2016-07-18 18:42:11 -0700

    Begin Post and add response to GET

--- routes.js
+++ routes.js
@@ -6,7 +6,17 @@ var router = express.Router();
 // GET /questions
 router.get("/", function(req, res) {
 
-    // Return All Questions
+    // Return All Questions - Stringify JSON
+    res.json({response: "You sent me a GET Request"});
+
+});
+
+// POST /questions
+router.get("/", function(req, res) {
+
+    // Return All Questions - Stringify JSON
+    res.json({response: "You sent me a GET Request"});
+
 });
 
 module.exports = router;
