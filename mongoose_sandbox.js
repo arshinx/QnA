85fadf7f2e788b77bc419fe24eabef6ee7295cd8
Arshin Jain <arshin.jain@outlook.com>
2016-07-19 13:58:06 -0700

    Semicolon

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -7,9 +7,9 @@ mongoose.connect("mongodb://localhost:27017/sandbox");
 var db = mongoose.connection;
 
 db.on("error", function(err) {
-    console.error("connection error:", err)
+    console.error("connection error:", err);
 });
 
 db.once("open", function(){
-    console.log("db connection succesful!")
+    console.log("db connection succesful!");
 });
