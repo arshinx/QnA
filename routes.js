b2aa29a5743fdf1579365c4c480e6a7ed5021793
Arshin Jain <arshin.jain@outlook.com>
2016-07-18 21:08:22 -0700

    Add Post request for answers

--- routes.js
+++ routes.js
@@ -17,7 +17,8 @@ router.post("/", function(req, res) {
 
     // Return All Questions - Stringify JSON
     res.json({
-        response: "You sent me a Post Request"
+        response: "You sent me a Post Request",
+        body: req.body
     });
 
 });
@@ -29,6 +30,19 @@ router.get("/:id", function(req, res) {
 
     // Return All Questions - Stringify JSON
     res.json({ response: "You sent me a GET Request for ID: " + req.params.id });
+
+});
+
+// POST /questions/:id/answers
+// Route for creating an answer
+router.post("/:id/answers", function(req, res) {
+
+    // Return All Questions - Stringify JSON
+    res.json({
+        response: "You sent me a Post Request to /answers",
+        questionID: "",
+        body: req.body
+    });
 
 });
 
