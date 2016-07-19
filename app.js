175fb4b04bf28a9436a7368e0ae843517deb7f2e
Arshin Jain <arshin.jain@outlook.com>
2016-07-18 17:08:47 -0700

    Adhere to DRY - Fcn as var

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ var express = require('express');
 var app = express();
 var jsonParser = require('body-parser').json;
 
-// Configure Middleware
-app.use(function(req, res, next) {
+// Configure Middleware - DRY Principle
+var jsonCheck = function(req, res, next) {
     if (req.body) {
         console.log("The Sky is", req.body.color);
     } else {
         console.log("There is no body property on the request");
     }
     next();
-});
+}
+
+app.use();
 
 // Use Modules with Express
 app.use(jsonParser());
