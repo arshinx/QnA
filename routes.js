429714a18fd79d1d1f7763248f2dcfac0ed2bec9
Arshin Jain <arshin.jain@outlook.com>
2016-07-18 20:40:52 -0700

    Get  Router for ID

--- routes.js
+++ routes.js
@@ -22,4 +22,14 @@ router.post("/", function(req, res) {
 
 });
 
+
+// GET /questions
+// Route for questions collection
+router.get("/:id", function(req, res) {
+
+    // Return All Questions - Stringify JSON
+    res.json({ response: "You sent me a GET Request for ID: " + req.params.id });
+
+});
+
 module.exports = router;
